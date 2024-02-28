5c94ab47dc91e69e35e3ddb3278a09cffeeff752
CHEGEBB <chegephil24@gmail.com>
2024-02-28 13:02:43 +0300

    binary.h

--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,7 @@
 #define _BINARY_TREES_H_
 
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -13,12 +14,14 @@
  */
 typedef struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
 } binary_tree_t;
 
+/* Function Prototypes */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *);
 
 #endif /* _BINARY_TREES_H_ */
