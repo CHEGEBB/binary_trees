1c4162b9f5e088fa4dc329da480f294955450b02
CHEGEBB <chegephil24@gmail.com>
2024-02-28 13:08:51 +0300

    added  to header

--- binary_trees.h
+++ binary_trees.h
@@ -22,6 +22,9 @@ typedef struct binary_tree_s
 
 /* Function Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *);
+
 
 #endif /* _BINARY_TREES_H_ */
