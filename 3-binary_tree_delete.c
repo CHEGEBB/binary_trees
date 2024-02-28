1da6748f3bb0132d6dce655037b084bde9419492
CHEGEBB <chegephil24@gmail.com>
2024-02-28 17:19:04 +0300

    task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+// Task: 3. Delete
+/* function that deletes an entire binary tree*/
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree: pointer to the root node of the tree to delete
+ * Return: void has no return
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return;
+    binary_tree_delete(tree->left);
+    binary_tree_delete(tree->right);
+    free(tree);
+}
+
