6ac402819f706a338ea74bea7f8a5fb8e34153ff
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2024-02-28 16:00:17 +0300

    Update 1-main.c

--- 1-main.c
+++ 1-main.c
@@ -16,8 +16,8 @@ int main(void)
     root->right = binary_tree_node(root, 402);
     binary_tree_print(root);
     printf("\n");
-    binary_tree_insert_right(root->left, 54);
-    binary_tree_insert_right(root, 128);
+    binary_tree_insert_left(root->right, 128);
+    binary_tree_insert_left(root, 54);
     binary_tree_print(root);
     return (0);
 }
