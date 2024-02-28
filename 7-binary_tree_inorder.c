ecdc35a503892f9477a4ac92e92f83fbc09b0b57
CHEGEBB <chegephil24@gmail.com>
2024-02-28 18:31:23 +0300

    update

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_inorder - Goes through a binary tree using in-order traversal.
+ * in-order traversal goes through a binary tree using in-order traversal.
+ * in in-order traversal, the left node is visited, then the root node, then the right node.
+ * @tree: Pointer to the root node of the tree to traverse.
+ * @func: Pointer to a function to call for each node.
+ * The value in the node must be passed as a parameter to this function.
+ * If tree or func is NULL, do nothing.
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+    /* Check if the tree or func is NULL */
+    if (tree == NULL || func == NULL)
+        return;
+
+    /* Go through the left node */
+    binary_tree_inorder(tree->left, func);
+    
+    /* Call the function pointer func with the value of the node */
+    func(tree->n);
+
+    /* Go through the right node */
+    binary_tree_inorder(tree->right, func);
+}
---
+++ 9-binary_tree_height.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ * The height of a binary tree is the number of edges between the tree's root and its furthest leaf.
+ * If tree is NULL, the function must return 0.
+ * @tree: Pointer to the root node of the tree to measure the height.
+ * Return: The height of the tree. If tree is NULL, the function must return 0.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    /* Declare left_height and right_height */
+    size_t left_height, right_height;
+
+/* If tree is NULL, return 0 */
+    if (tree == NULL)
+        return (0);
+
+    /* If tree has no left or right child, return 0 */
+    if (tree->left == NULL && tree->right == NULL)
+        return (0);
+        
+    /* Recursively call binary_tree_height on the left and right children of the tree */
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
+
+    /* If left_height is greater than right_height, return left_height + 1 */
+    if (left_height > right_height)
+        return (left_height + 1);
+    return (right_height + 1);
+}
