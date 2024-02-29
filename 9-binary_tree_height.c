b0486ec86010ee5f18bc05ffd0f0f556f6106602
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2024-02-29 16:17:11 +0300

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -20,14 +20,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	/* If tree has no left or right child, return 0 */
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
-	    
-	/* Recursively call binary_tree_height on the left and right children of the tree */
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
 	/* If left_height is greater than right_height, return left_height + 1 */
 	if (left_height > right_height)
 		return (left_height + 1);
-	
 	return (right_height + 1);
 }
