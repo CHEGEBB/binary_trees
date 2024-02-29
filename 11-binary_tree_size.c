1d05f637efec2ce81112ca13fa60fef6d253baf9
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2024-02-29 16:20:49 +0300

    Update 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,20 +8,19 @@
  * @tree: Pointer to the root node of the tree to measure the size
  * Return: The size of the tree. If tree is NULL, the function must return 0.
  */
-
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    /* Declare left_size and right_size */
-    size_t left_size, right_size;
+	/* Declare left_size and right_size */
+	size_t left_size, right_size;
 
-    /* If tree is NULL, return 0 */
-    if (tree == NULL)
-        return (0);
+	/* If tree is NULL, return 0 */
+	if (tree == NULL)
+		return (0);
 
-    /* Recursively call binary_tree_size on the left and right children of the tree */
-    left_size = binary_tree_size(tree->left);
-    right_size = binary_tree_size(tree->right);
+	/* Recursively call binary_tree_size on the left and right children of the tree */
+	left_size = binary_tree_size(tree->left);
+	right_size = binary_tree_size(tree->right);
 
-    /* Return the sum of left_size, right_size, and 1 */
-    return (left_size + right_size + 1);
+	/* Return the sum of left_size, right_size, and 1 */
+	return (left_size + right_size + 1);
 }
