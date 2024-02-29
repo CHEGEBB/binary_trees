f3ff5c2f984e22655c295a8600d0df1c2cef6274
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2024-02-29 16:45:20 +0300

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,7 +8,8 @@
 
 size_t depth(const binary_tree_t *tree);
 const binary_tree_t *get_leaf(const binary_tree_t *tree);
-int is_perfect_recursive(const binary_tree_t *tree, size_t leaf_depth, size_t level);
+int is_perfect_recursive(const binary_tree_t *tree,
+                         size_t leaf_depth, size_t level);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 unsigned char is_leaf(const binary_tree_t *node);
 
