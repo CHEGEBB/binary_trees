f9cd05e61fa0623ab94485b5918127d37ff9da2e
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2024-02-29 16:36:53 +0300

    Update 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,17 +10,17 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    /* if tree is NULL, return 0 */
-    if (tree == NULL)
-        return (0);
+	/* if tree is NULL, return 0 */
+	if (tree == NULL)
+		return (0);
 
-    /* if tree is a leaf, return 1 */
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
+	/* if tree is a leaf, return 1 */
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-    /* if tree has two children, check if both are full */
-    if (tree->left != NULL && tree->right != NULL)
-        return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	/* if tree has two children, check if both are full */
+	if (tree->left != NULL && tree->right != NULL)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 
-    return (0);
+	return (0);
 }
