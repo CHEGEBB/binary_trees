71348e499740caf31c33c3b32e54934a13289cbd
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2024-03-01 18:17:35 +0300

    Update 124-sorted_array_to_avl.c

--- 124-sorted_array_to_avl.c
+++ 124-sorted_array_to_avl.c
@@ -21,22 +21,22 @@
  */
 avl_t *aux_sort(avl_t *parent, int *array, int begin, int last)
 {
-    avl_t *root;
-    binary_tree_t *aux;
-    int mid = 0;
+	avl_t *root;
+	binary_tree_t *aux;
+	int mid = 0;
 
-    if (begin <= last)
-    {
-        mid = (begin + last) / 2;
-        aux = binary_tree_node((binary_tree_t *)parent, array[mid]);
-        if (aux == NULL)
-            return (NULL);
-        root = (avl_t *)aux;
-        root->left = aux_sort(root, array, begin, mid - 1);
-        root->right = aux_sort(root, array, mid + 1, last);
-        return (root);
-    }
-    return (NULL);
+	if (begin <= last)
+	{
+		mid = (begin + last) / 2;
+		aux = binary_tree_node((binary_tree_t *)parent, array[mid]);
+		if (aux == NULL)
+			return (NULL);
+		root = (avl_t *)aux;
+		root->left = aux_sort(root, array, begin, mid - 1);
+		root->right = aux_sort(root, array, mid + 1, last);
+		return (root);
+	}
+	return (NULL);
 }
 
 /**
@@ -55,7 +55,7 @@ avl_t *aux_sort(avl_t *parent, int *array, int begin, int last)
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    if (array == NULL || size == 0)
-        return (NULL);
-    return (aux_sort(NULL, array, 0, (int)size - 1));
+	if (array == NULL || size == 0)
+		return (NULL);
+	return (aux_sort(NULL, array, 0, (int)size - 1));
 }
