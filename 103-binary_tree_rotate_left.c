86c3d9c40a9dc669c3ce8576334770ff480c16c8
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2024-03-01 15:58:56 +0300

    Update 103-binary_tree_rotate_left.c

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -15,21 +15,20 @@
  * Return:
  * Pointer to the new root node of the tree after rotation.
  */
-
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-    binary_tree_t *new_root;
+	binary_tree_t *new_root;
 
-    if (tree == NULL || tree->right == NULL)
-        return (NULL);
+	if (tree == NULL || tree->right == NULL)
+		return (NULL);
 
-    new_root = tree->right;
-    tree->right = new_root->left;
-    if (new_root->left != NULL)
-        new_root->left->parent = tree;
-    new_root->left = tree;
-    new_root->parent = tree->parent;
-    tree->parent = new_root;
+	new_root = tree->right;
+	tree->right = new_root->left;
+	if (new_root->left != NULL)
+		new_root->left->parent = tree;
+	new_root->left = tree;
+	new_root->parent = tree->parent;
+	tree->parent = new_root;
 
-    return (new_root);
+	return (new_root);
 }
