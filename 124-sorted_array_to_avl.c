c6cb5d4b6b7c6fbf4afaf99aa23d6c48a4473050
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2024-03-01 18:06:57 +0300

    Update 124-sorted_array_to_avl.c

--- 124-sorted_array_to_avl.c
+++ 124-sorted_array_to_avl.c
@@ -1,11 +1,24 @@
+#include <stdlib.h>
 #include "binary_trees.h"
+#include <stdio.h>
+#include <stddef.h>
+
 /**
- * aux_sort - create the tree using the half element of the array
- * @parent: parent of the node to create
- * @array: sorted array
- * @begin: position where the array starts
- * @last: position where the array ends
- * Return: tree created
+ * aux_sort - Recursively constructs an AVL tree from a sorted array.
+ * @parent: Parent node of the current subtree being constructed.
+ * @array: Pointer to the sorted array.
+ * @start_index: Start index of the current subtree in the array.
+ * @end_index: End index of the current subtree in the array.
+ *
+ * Description:
+ * This function recursively constructs an AVL tree from a sorted array. It starts
+ * by finding the middle element of the array and creates a node for it. Then, it
+ * recursively builds the left and right subtrees using the elements to the left and
+ * right of the middle element, respectively.
+ *
+ * Return:
+ * - Pointer to the root node of the constructed AVL tree.
+ * - NULL if the start index is greater than the end index.
  */
 avl_t *aux_sort(avl_t *parent, int *array, int begin, int last)
 {
@@ -27,10 +40,18 @@ avl_t *aux_sort(avl_t *parent, int *array, int begin, int last)
 	return (NULL);
 }
 /**
- * sorted_array_to_avl - create a alv tree from sorted array
- * @array: sorted array
- * @size: size of the sorted array
- * Return: alv tree form sorted array
+ * sorted_array_to_avl - Constructs an AVL tree from a sorted array.
+ * @array: Pointer to the first element of the sorted array.
+ * @size: Number of elements in the array.
+ *
+ * Description:
+ * This function constructs an AVL tree from a sorted array. It takes a sorted
+ * array and its size as input and creates an AVL tree using the AVL insertion
+ * algorithm. The array is assumed to be sorted in ascending order.
+ *
+ * Return:
+ * - Pointer to the root node of the constructed AVL tree if successful.
+ * - NULL if the array is NULL or empty.
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
