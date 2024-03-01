553c76bb3ee338ce006e6fc2c5d936f797e8b42d
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2024-03-01 19:33:52 +0300

    Update 131-heap_insert.c

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -19,14 +19,14 @@ bst_t *swap(bst_t *a, bst_t *b)
 }
 
 /**
- * convert - Converts a number from base 10 to another base and returns the string representation.
- * This function takes an input number (num) in base 10 and converts it to the specified base,
- * generating a string representation of the converted number. The base can be any integer value
- * greater than 1. Additionally, if the lowercase flag is set to 1, hexadecimal values will be
+ * convert - Converts a number from base 10
+ * This function takes an input number (num) in base 10
+ * generating a string representation of the converted number.
+ * greater than 1. Additionally, if the lowercase flag is set to 1
  * represented in lowercase letters; otherwise, they will be uppercase.
  * @num: The input number to be converted.
  * @base: The base to convert the number to.
- * @lowercase: A flag indicating whether hexa values should be lowercase (1) or uppercase (0).
+ * @lowercase: A flag indicating whether hexa values
  * Return: A pointer to the string representation of the converted number.
  */
 char *convert(unsigned long int num, int base, int lowercase)
@@ -36,8 +36,8 @@ char *convert(unsigned long int num, int base, int lowercase)
 
 /**
  * binary_tree_size - Measures the size of a binary tree.
- * This function calculates the number of descendant nodes (both children and grandchildren)
- * in a binary tree rooted at the given node. It recursively traverses the tree and counts
+ * This function calculates the number of descendant nodes
+ * in a binary tree rooted at the given node.
  * each node encountered.
  * @tree: Pointer to the root node of the binary tree.
  * Return: The number of descendant nodes in the binary tree.
