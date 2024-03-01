92f24fd564caa4f42347f2bdc85c5a7fc6853226
CHEGEBB <chegephil24@gmail.com>
2024-03-01 13:36:48 +0300

    update

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -4,90 +4,85 @@
 #include <stdio.h>
 #define INIT_NODE {0, NULL, NULL, NULL}
 
-#include "binary_trees.h"
-
 /**
- * swap - Swaps two nodes in a binary tree.
- * @a: Pointer to the first node.
- * @b: Pointer to the second node.
- * This function swaps the positions of two nodes in a binary tree.
- * @return: Pointer to the root node of the tree after swapping.
+ * swap - Swaps the positions of two nodes in a binary tree.
+ * This function swaps the positions of two nodes in a binary tree. It takes
+ * two pointers to nodes (a and b) as arguments and swaps their positions. The
+ * function returns a pointer to the root node of the tree after the swap.
+ * @a: Pointer to the first node to be swapped.
+ * @b: Pointer to the second node to be swapped.
+ * Return: Pointer to the root node of the tree after the swap.
  */
-binary_tree_t *swap(binary_tree_t *a, binary_tree_t *b)
+
+bst_t *swap(bst_t *a, bst_t *b)
 {
-    binary_tree_t *temp;
+	bst_t a_copy = INIT_NODE;
 
-    temp = a->parent;
-    a->parent = b->parent;
-    b->parent = temp;
+	/* Copy the values of a into a_copy */
+	a_copy.n = a->n;
+	a_copy.parent = a->parent;
+	a_copy.left = a->left;
+	a_copy.right = a->right;
+	a->parent = b;
+	a->left = b->left;
+	a->right = b->right;
 
-    if (a->parent != NULL)
-    {
-        if (a->parent->left == b)
-            a->parent->left = a;
-        else
-            a->parent->right = a;
-    }
+	/* Update the parent of b's left child */
+	if (b->left)
+		b->left->parent = a;
+	if (b->right)
+		b->right->parent = a;
 
-    if (b->parent != NULL)
-    {
-        if (b->parent->left == a)
-            b->parent->left = b;
-        else
-            b->parent->right = b;
-    }
-
-    return (a->parent == NULL ? a : b);
-}
-
-/**
- * heap_insert - Inserts a value into a Max Binary Heap.
- * @root: Double pointer to the root node of the Heap to insert the value.
- * @value: Value to store in the node to be inserted.
- * Return: Pointer to the created node, or NULL on failure.
- */
-heap_t *heap_insert(heap_t **root, int value)
-{
-    heap_t *new_node, *inserted_node;
-
-    if (root == NULL)
-        return NULL;
-
-    new_node = binary_tree_node(NULL, value);
-    if (new_node == NULL)
-        return NULL;
-
-    if (*root == NULL)
-    {
-        *root = new_node;
-        return new_node;
-    }
-
-    inserted_node = binary_tree_insert_left(*root, value);
-    if (inserted_node == NULL)
-    {
-        free(new_node);
-        return NULL;
-    }
-
-    return swap(inserted_node, inserted_node->parent);
+	/* Copy the values of b into a */
+	b->parent = a_copy.parent;
+	if (a_copy.parent)
+	{
+		if (a == a_copy.parent->left)
+			a_copy.parent->left = b;
+		else
+			a_copy.parent->right = b;
+	}
+	/* Update the parent of a's left child */
+	if (b == a_copy.left)
+	{
+		b->left = a;
+		b->right = a_copy.right;
+		if (a_copy.right)
+			a_copy.right->parent = b;
+	}
+	/* Update the parent of a's right child */
+	else if (b == a_copy.right)
+	{
+		b->right = a;
+		b->left = a_copy.left;
+		if (a_copy.left)
+			a_copy.left->parent = b;
+	}
+	while (b->parent)
+		b = b->parent;
+	return (b);
 }
 
 
 /**
- * convert - converts number and base into string
- * @num: input number
- * @base: input base
- * @lowercase: flag if hexa values need to be lowercase
- * Return: result string
+ * convert - Converts a number from base 10 to another base and returns the string representation.
+ * This function takes an input number (num) in base 10 and converts it to the specified base,
+ * generating a string representation of the converted number. The base can be any integer value
+ * greater than 1. Additionally, if the lowercase flag is set to 1, hexadecimal values will be
+ * represented in lowercase letters; otherwise, they will be uppercase.
+ * @num: The input number to be converted.
+ * @base: The base to convert the number to.
+ * @lowercase: A flag indicating whether hexa values should be lowercase (1) or uppercase (0).
+ * Return: A pointer to the string representation of the converted number.
  */
- 
+
 char *convert(unsigned long int num, int base, int lowercase)
 {
 	static char *rep;
 	static char buffer[50];
 	char *ptr;
 
+	/* Set the representation to lowercase or uppercase */
 	rep = (lowercase)
 		? "0123456789abcdef"
 		: "0123456789ABCDEF";
@@ -103,14 +98,16 @@ char *convert(unsigned long int num, int base, int lowercase)
 
 
 /**
- * binary_tree_size - measures the size of a binary tree
- * @tree: input binary tree
- * Return: number of descendant child nodes
+ * binary_tree_size - Measures the size of a binary tree.
+ * This function calculates the number of descendant nodes (both children and grandchildren)
+ * in a binary tree rooted at the given node. It recursively traverses the tree and counts
+ * each node encountered.
+ * @tree: Pointer to the root node of the binary tree.
+ * Return: The number of descendant nodes in the binary tree.
  */
- 
- 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	/* If the tree is empty, return 0 */
 	if (!tree)
 		return (0);
 
@@ -119,20 +116,26 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 
 /**
- * insert - helper func to insert node to correct location
- * @root: double pointer to root of max heap
- * @node: node to insert
+ * insert - Inserts a node into the correct location in a Max Heap.
+ * This function is a helper function that inserts a node into the correct
+ * location in a Max Heap. It takes a double pointer to the root of the heap
+ * and a pointer to the node to be inserted as arguments.
+ * @root: Double pointer to the root of the Max Heap.
+ * @node: Pointer to the node to be inserted.
  */
+
  
  
 void insert(heap_t **root, heap_t *node)
 {
+	/* Create a binary string representation of the heap size */
 	heap_t *tmp;
 	int size;
 	unsigned int i;
 	char *binary;
 	char c;
 
+	/* Find the parent of the new node */
 	tmp = *root;
 	size = binary_tree_size(tmp) + 1;
 	binary = convert(size, 2, 1);
@@ -163,17 +166,23 @@ void insert(heap_t **root, heap_t *node)
 
 
 /**
- * heap_insert - inserts a value in Max Binary Heap
- * @root: double pointer to root of tree
- * @value: input value
- * Return: pointer to the created node, or NULL on failure
+ * heap_insert - Inserts a value into a Max Binary Heap.
+ * This function inserts a value into a Max Binary Heap rooted at the given
+ * root node. It takes a double pointer to the root of the tree and the value
+ * to be inserted as arguments. If successful, it returns a pointer to the
+ * newly created node; otherwise, it returns NULL.
+ * @root: Double pointer to the root of the Max Binary Heap.
+ * @value: The value to be inserted into the heap.
+ * Return: Pointer to the created node, or NULL on failure.
  */
+
  
  
 heap_t *heap_insert(heap_t **root, int value)
 {
 	heap_t *ht = NULL, *ret;
 
+	/* If the root is NULL, create a new node and set it as the root */
 	if (!root)
 		return (NULL);
 	ht = calloc(1, sizeof(heap_t));
