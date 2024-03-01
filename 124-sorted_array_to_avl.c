24ea423bad9bf8140d99398916527a52c075b204
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2024-03-01 18:05:15 +0300

    Update 124-sorted_array_to_avl.c

--- 124-sorted_array_to_avl.c
+++ 124-sorted_array_to_avl.c
@@ -1,20 +1,11 @@
 #include "binary_trees.h"
 /**
- * aux_sort - Recursively constructs an AVL tree from a sorted array.
- * @parent: Parent node of the current subtree being constructed.
- * @array: Pointer to the sorted array.
- * @start_index: Start index of the current subtree in the array.
- * @end_index: End index of the current subtree in the array.
- *
- * Description:
- * This function recursively constructs an AVL tree from a sorted array. It starts
- * by finding the middle element of the array and creates a node for it. Then, it
- * recursively builds the left and right subtrees using the elements to the left and
- * right of the middle element, respectively.
- *
- * Return:
- * - Pointer to the root node of the constructed AVL tree.
- * - NULL if the start index is greater than the end index.
+ * aux_sort - create the tree using the half element of the array
+ * @parent: parent of the node to create
+ * @array: sorted array
+ * @begin: position where the array starts
+ * @last: position where the array ends
+ * Return: tree created
  */
 avl_t *aux_sort(avl_t *parent, int *array, int begin, int last)
 {
