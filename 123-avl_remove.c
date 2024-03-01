b5f6567d55ee3a14ce9c1db66fa02468b547f3a7
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2024-03-01 16:56:41 +0300

    Update 123-avl_remove.c

--- 123-avl_remove.c
+++ 123-avl_remove.c
@@ -1,4 +1,6 @@
 #include "binary_trees.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * avl_remove - Removes a node from an AVL tree.
@@ -8,59 +10,59 @@
  */
 avl_t *avl_remove(avl_t *root, int value)
 {
-    if (root == NULL)
-        return NULL;
+	if (root == NULL)
+		return NULL;
 
-    if (value < root->n)
-    {
-        root->left = avl_remove(root->left, value);
-    }
-    else if (value > root->n)
-    {
-        root->right = avl_remove(root->right, value);
-    }
-    else
-    {
-        if (root->left == NULL)
-        {
-            avl_t *temp = root->right;
-            free(root);
-            return temp;
-        }
-        else if (root->right == NULL)
-        {
-            avl_t *temp = root->left;
-            free(root);
-            return temp;
-        }
+	if (value < root->n)
+	{
+		root->left = avl_remove(root->left, value);
+	}
+	else if (value > root->n)
+	{
+		root->right = avl_remove(root->right, value);
+	}
+	else
+	{
+		if (root->left == NULL)
+		{
+			avl_t *temp = root->right;
+			free(root);
+			return temp;
+		}
+		else if (root->right == NULL)
+		{
+			avl_t *temp = root->left;
+			free(root);
+			return temp;
+		}
 
-        avl_t *temp = root->right;
-        while (temp->left != NULL)
-            temp = temp->left;
+		avl_t *temp = root->right;
+		while (temp->left != NULL)
+			temp = temp->left;
 
-        root->n = temp->n;
-        root->right = avl_remove(root->right, temp->n);
-    }
+		root->n = temp->n;
+		root->right = avl_remove(root->right, temp->n);
+	}
 
-    int balance = avl_balance_factor(root);
+	int balance = avl_balance_factor(root);
 
-    if (balance > 1 && avl_balance_factor(root->left) >= 0)
-        return avl_rotate_right(root);
+	if (balance > 1 && avl_balance_factor(root->left) >= 0)
+		return avl_rotate_right(root);
 
-    if (balance < -1 && avl_balance_factor(root->right) <= 0)
-        return avl_rotate_left(root);
+	if (balance < -1 && avl_balance_factor(root->right) <= 0)
+		return avl_rotate_left(root);
 
-    if (balance > 1 && avl_balance_factor(root->left) < 0)
-    {
-        root->left = avl_rotate_left(root->left);
-        return avl_rotate_right(root);
-    }
+	if (balance > 1 && avl_balance_factor(root->left) < 0)
+	{
+		root->left = avl_rotate_left(root->left);
+		return avl_rotate_right(root);
+	}
 
-    if (balance < -1 && avl_balance_factor(root->right) > 0)
-    {
-        root->right = avl_rotate_right(root->right);
-        return avl_rotate_left(root);
-    }
+	if (balance < -1 && avl_balance_factor(root->right) > 0)
+	{
+		root->right = avl_rotate_right(root->right);
+		return avl_rotate_left(root);
+	}
 
-    return root;
+	return root;
 }
