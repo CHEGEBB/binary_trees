c7a90d846d92af8d2c84d00298f113e813e6f5c7
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2024-03-01 16:22:40 +0300

    Update 113-bst_search.c

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -6,20 +6,20 @@
  * @tree: Pointer to the root node of the BST to search.
  * @value: Value to search for in the tree.
  * Description:
- * This function searches for a value in a Binary Search Tree (BST). It returns
- * a pointer to the node containing the value, or NULL if the value is not found.
+ * This function searches for a value in a Binary Search Tree (BST).
+ * a pointer to the node containing the value, or NULL
  * Return: Pointer to the node containing the value, or NULL if not found.
  */
 
 bst_t *bst_search(const bst_t *tree, int value)
 {
-    if (!tree)
-        return (NULL);
+	if (!tree)
+		return (NULL);
 
-    if (value == tree->n)
-        return ((bst_t *)tree);
+	if (value == tree->n)
+		return ((bst_t *)tree);
 
-    if (value < tree->n)
-        return (bst_search(tree->left, value));
-    return (bst_search(tree->right, value));
+	if (value < tree->n)
+		return (bst_search(tree->left, value));
+	return (bst_search(tree->right, value));
 }
